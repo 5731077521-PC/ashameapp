34eb7104f337614fb03d890b855a4eb5e8ecedc5
netklause <netklause@gmail.com>
2017-07-25 20:47:23 +0900

    pipelining

--- Desktop/pysource/testgit/assemble1.py
+++ Desktop/pysource/testgit/assemble1.py
@@ -3,20 +3,25 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import time
 
+dumpFolder = "dump_3"+"/"
+dumpDir = "/home/netklause/Desktop/data/sandbox/"+dumpFolder
+
 
 def procedure(capture):
+
 	argusFile = capture + '.argus'
 	rawInputFile = capture + '_raw.csv'
 	cleanInputFile = capture + '_clean.csv'
 	cleanInputDir = "~/Desktop/data/sandbox/"
 	generatedProb = capture + "_prob.csv"
 	data_cleaner_name = "data_cleaner_beta"
-	dumpDir = "~/Desktop/data/sandbox/dump/"
+	#dumpDir = "~/Desktop/data/sandbox/dump_2/"
 	prob_dumpDir = "~/Desktop/data/sandbox/prob_dump/"
 
 	
 	os.system("argus -r " + dumpDir+capture + " -w " + dumpDir+argusFile)
 	time.sleep(2)
+	#time.sleep(1)
 	#os.system("ra -r "+dumpDir+argusFile)
 	os.system("ra -n -c, -r "+
 		dumpDir+argusFile+
@@ -35,7 +40,7 @@ def procedure(capture):
 		dumpDir+cleanInputFile)
 	time.sleep(1)
 	os.system("python parsing_procedure.py "+dumpDir+cleanInputFile+" "+prob_dumpDir+generatedProb) 
-	print("done")
+	
 
 
 
@@ -48,7 +53,7 @@ class NewCreatedFileHandler(FileSystemEventHandler):
         	time.sleep(2)
         	procedure(createdFileName)
         else:
-        	print("Got event for file " , str(event.src_path)[42:])
+        	print("Got event for file " , str(event.src_path)[44:]) #42
       		
 
 
@@ -58,10 +63,10 @@ def main():
 	observer = Observer()
 	event_handler = NewCreatedFileHandler() # create event handler
 	# set observer to use created handler in directory
-	observer.schedule(event_handler, path='/home/netklause/Desktop/data/sandbox/dump')
+	observer.schedule(event_handler, path=dumpDir)
 	observer.start()
 
-	os.system("tshark -n -i any -a filesize:3 -b files:10 -w /home/netklause/Desktop/data/sandbox/dump/capture")
+	os.system("tshark -n -i any -a filesize:5 -b files:500 -w "+dumpDir+"capture")
 
 	# sleep until keyboard interrupt, then stop + rejoin the observer
 	try:
